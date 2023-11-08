b555ee780c03d16ec0fa8fe564d3f140fd3c615e
Javed Bagwan <javed460@gmail.com>
2023-11-08 10:00:00 +0530

    added controller

---
+++ src/main/java/com/example/mockitotests/controller/OrderController.java
@@ -0,0 +1,9 @@
+package com.example.mockitotests.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderController {
+}
